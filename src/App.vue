a078bf399aa30a846673c6e2d742bfb811b48e1b
Oleksandr Liakhov <Oleksandr_Liakhov@epam.com>
2019-02-06 12:15:33 +0200

    moved counter into user-list component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,8 @@
 <template>
 	<div id="app">
-		<nav-bar></nav-bar>
+		<nav-bar />
 		<main>
-			<section>
-				<a class="load-users" href="#" @click.prevent="loadUsers">Load users</a>
-				<h2>Count: {{userCount}}</h2>
-			</section>
-			<user-list></user-list>
+			<user-list />
 		</main>
 	</div>
 </template>
@@ -15,26 +11,12 @@
 	import navBar from '@/components/nav-bar.vue';
 	import userList from '@/components/user-list.vue';
 
-	import {mapGetters} from 'vuex';
-
 	export default {
 		name: 'App',
 
 		components: {
 			navBar,
 			userList
-		},
-
-		computed: {
-			...mapGetters('users', ['userCount'])
-		},
-
-		methods: {
-			loadUsers() {
-				this.$store.dispatch('users/LOAD_USERS').catch(
-					err => console.log('--- ERROR --->', err.message)
-				)
-			}
 		}
 	}
 </script>
@@ -52,20 +34,5 @@
 		min-height: 100%;
 		margin: auto;
 		padding: 76px 12px 32px;
-
-		section {
-			display: flex;
-			justify-content: space-between;
-			align-items: center;
-
-			.load-users {
-				font-size: 16px;
-				color: $vue-green-plus-25;
-			}
-
-			h2 {
-				margin-left: auto;
-			}
-		}
 	}
 </style>
--- src/components/user-list.vue
+++ src/components/user-list.vue
@@ -1,32 +1,41 @@
 ﻿<template>
-	<ul class="user-list">
-		<li class="user-list--item" v-for="user in users" :index="user.id">
-			<div class="avatar">
-				<img :src="avatarUrl" width="48" />
-			</div>
-			<span class="username">{{user.name}}</span>
-			<a href="#" class="btn-remove" @click.prevent="removeUser(user.id)">+</a>
-		</li>
-	</ul>
+	<div>
+		<section>
+			<a class="load-users" href="#" @click.prevent="loadUsers">Load users</a>
+			<h2>Count: {{userCount}}</h2>	
+		</section>
+
+		<ul class="user-list">
+			<li class="user-list--item" v-for="user in users" :index="user.id">
+				<div class="avatar">
+					<img src="@/assets/user-avatar.png" width="48" />
+				</div>
+				<span class="username">{{user.name}}</span>
+				<a href="#" class="btn-remove" @click.prevent="removeUser(user.id)">+</a>
+			</li>
+		</ul>
+	</div>
 </template>
 
 <script>
-	import {mapState} from 'vuex';
+	import userAvatar from '@/assets/user-avatar.png';
+	import {mapState, mapGetters} from 'vuex';
 
 	export default {
 		name: 'UserList',
 
-		data() {
-			return {
-				avatarUrl: 'https://bit.ly/2TpLp1e'
-			}
-		},
-
 		computed: {
-			...mapState('users', ['users'])
+			...mapState('users', ['users']),
+			...mapGetters('users', ['userCount'])
 		},
 
 		methods: {
+			loadUsers() {
+				this.$store.dispatch('users/LOAD_USERS').catch(
+					err => console.log('--- ERROR --->', err.message)
+				)
+			},
+
 			removeUser(id) {
 				this.$store.commit('users/REMOVE_USER', id);
 			}
@@ -35,6 +44,22 @@
 </script>
 
 <style lang="scss" scoped>
+
+	section {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+
+		.load-users {
+			font-size: 16px;
+			color: $vue-green-plus-25;
+		}
+
+		h2 {
+			margin-left: auto;
+		}
+	}
+
 	.user-list {
 		margin-bottom: 16px;
 		background: $vue-dark-minus-5;
